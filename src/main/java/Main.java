ea9539ef47032f7ca51b2beced285dcc4c80f7d1
Cherchok <viktor.yurzanov@gmail.com>
2018-02-07 21:55:56 +0300

    new class was created to show what Enum is

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,15 +6,24 @@ public class Main {
         long l = i; // неявное приведение типов
         double d = i; // неявное приведение типов
         short s = (short) i; // явное приведение типов
-        System.out.println("int"+i+" long"+l+" double"+d +"short"+s);
+        System.out.println("int "+i+" long "+l+" double "+d +"short "+s);
         System.out.println(MethodClass.factorial(5));
         MethodClass methodClass = new MethodClass();
-        int[] arr = new int[]{1,5,7,9};
+        int[] arr = new int[]{1,5,7,9,8};
         System.out.println(Arrays.toString(new int[]{methodClass.search(arr, 9)}));
         System.out.println(methodClass.Sum());
 
         String str = "Hi friends. You dont expect me!))";
         methodClass.changeIt(str);
         System.out.println(str);
+        System.out.println(methodClass.changeIt(str));
+        int[] arr2 = new int[]{1,5,7,8,9,12,13,14,15,16};
+        System.out.println(Arrays.toString(arr2));
+       // methodClass.DelElArr(arr2,7);
+        System.out.println(methodClass.ArrSearch(arr2,13));
+        System.out.println(Arrays.binarySearch(arr2, 13));
+        EnumExample season = EnumExample.SUMMER;
+        System.out.println(season);
+        
     }
 }
--- src/main/java/MethodClass.java
+++ src/main/java/MethodClass.java
@@ -1,19 +1,20 @@
 
 public class MethodClass {
-    int a;
-    int b;
-    int c;
+    private int a;
+    private int b;
+    private int c;
+
     // попрактикуемся с конструктором,  что это для чего он нужен.
     // он нужен для инициализации полей
-    MethodClass(){
-        a=10;
-        b=10;
-        c=100;
+    MethodClass() {
+        a = 10;
+        b = 10;
+        c = 100;
     }
 
     // создадим метод для конструктора
-    protected int Sum(){
-        return a*b*c;
+    protected int Sum() {
+        return a * b * c;
     }
 
     // search method for arrays
@@ -27,42 +28,83 @@ public class MethodClass {
     }
 
     // visa versa method for arrays
-    public int[] recursion(int[] array){
-        for (int i = 0; i <array.length/2 ; i++) {
+    public int[] recursion(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
             int temp = array[i];
-            array[i] = array[array.length-1-i];
-            array[array.length-1-i] = temp;
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
         }
         return array;
     }
 
     // concatenation method for arrays
-    public int[] concatArray(int[] array1, int[] array2){
+    public int[] concatArray(int[] array1, int[] array2) {
 
-        if(array1==null){
+        if (array1 == null) {
             return array2;
         }
-        if(array2==null){
+        if (array2 == null) {
             return array1;
         }
-        int [] array12 = new int[array1.length + array2.length];
-        System.arraycopy(array1,0,array12,0,array2.length);
-        System.arraycopy(array2,0,array12, array1.length,array2.length);
+        int[] array12 = new int[array1.length + array2.length];
+        System.arraycopy(array1, 0, array12, 0, array2.length);
+        System.arraycopy(array2, 0, array12, array1.length, array2.length);
         return array12;
     }
 
     // recursion method factorial
-    public static int factorial(int n){
-        if(n==1){
+    public static int factorial(int n) {
+        if (n == 1) {
             return 1;
         }
-        return n*factorial(n-1);
+        return n * factorial(n - 1);
     }
 
     // метод менающий строку
-    public String changeIt(String str){
+    public String changeIt(String str) {
         str = "Hello";
         return str;
     }
 
+    // delete element of array
+    public int[] DelElArr(int[] arr, int searchKey) {
+        int numEl = arr.length;
+        int i;
+        for (i = 0; i < numEl; i++) {
+            if (arr[i] == searchKey) {
+                break;
+            }
+        }
+        for (i = 0; i < numEl; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        return arr;
+    }
+
+    // binary search
+    public int ArrSearch(int[] arr, int searchKey) {
+        int curIn;
+        int numEl = arr.length;
+        int down = 0;
+        int up = numEl - 1;
+
+        while (true) {
+            curIn = (down + up) / 2;
+            if (arr[curIn] == searchKey) {
+
+                return curIn;
+            }
+            else if (down > up) {
+                return numEl;
+            } else {
+                if (arr[curIn] < searchKey) {
+                     down = curIn + 1;
+                }
+                else
+                    up = curIn - 1;
+            }
+        }
+    }
+
 }
+
